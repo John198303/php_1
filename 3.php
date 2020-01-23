ba5f6f484e35cffb210f57bd22ea370a17e1babf
John198303 <potekhine@gmail.com>
2020-01-23 19:02:21 +0300

    2

--- 3.php
+++ 3.php
@@ -1,4 +1,5 @@
 <?php
+// Первое
 $i = 0;
 
 while ($i <= 100){
@@ -7,6 +8,8 @@ while ($i <= 100){
 	}
 	$i++;
 }
+
+// Второе
 
 $a = 0;
 
@@ -26,3 +29,42 @@ do {
 	$a++;
 	
 }while($a <= 10);
+
+// Таблица со *
+
+echo '<table border="1">';
+
+for ($i = 0; $i <= 15; $i++){
+	
+	echo '<tr>';
+	
+	for ($j = 0; $j <= 15; $j++){
+		
+		if ($i == 0){
+			
+			echo '<td style bgcolor = "gray">' .($i + 1) * $j . '</td>' ;
+			
+		}elseif ($i > 0 and $j == 0){
+			
+			$j++;			
+			
+			echo '<td style bgcolor = "gray">' . $i * $j . '</td>';
+			
+			$j--;
+			
+		}else{
+			
+			echo '<td>' .  $i * $j . '</td>';
+		}
+		
+	}
+	echo '<br>';
+	echo '</tr>';
+}
+
+echo '</table>';
+
+
+
+
+
