c3ac880f5e66442d24024ad6bd765aa93d62859a
John198303 <potekhine@gmail.com>
2020-01-28 12:28:53 +0300

    2

--- 4.php
+++ 4.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<html>
+ <head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/style.css">  
+  <title>Photo</title>    
+ </head>
+ <body> 
+<?php
+$dir = "img/";
+$files = scandir($dir);
+	foreach($files as $file){
+			if ($file == '.' || $file == '..'){
+				continue;
+			}  			
+			echo '<a href="#myModal"  id="myBtn"> <img src=' . "img/" . $file . ' " width="50" height="50" /></a>';
+			echo '<div id="myModal" class="modal">
+					<div class="modal-content">
+						<span class="close">&times;</span>
+						<img src=' . "img/" . $file . '>
+					 </div>
+				  </div>';
+    }    
+?>
+  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+  <script src="js/script.js"></script> 
+ </body>
 </html>
